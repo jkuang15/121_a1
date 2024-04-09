6cd5e889f1be2c41133173c4bbe9c3a3c489bcb5
Jamie Kuang <jkuang9@uci.edu>
2024-04-08 19:32:12 -0700

    added all files (had to redo repo)

---
+++ PartA.py
@@ -0,0 +1,18 @@
+# test
+class Tokenizer:
+    def __init__(self):
+        self.dictionary = dict()
+
+    def tokenize(TextFilePath):
+        raise NotImplementedError
+    
+    def computeWordFrequencies(tokenList):
+        # if token in self dict inrement by 1
+        raise NotImplementedError
+    
+    def printFrequencies(wordCount):
+        raise NotImplementedError
+
+if __name__ == "__main__":
+    raise NotImplementedError
+        
---
+++ README.txt
@@ -0,0 +1,1 @@
+Assignment 1: Text Processing - Jamie Kuang - jkuang9
