dd0948f3481cb997e2e6b6b0ee1b9a6c314fc9c7
Jamie Kuang <jkuang9@uci.edu>
2024-04-12 12:53:01 -0700

    added comments and error handling

--- PartA.py
+++ PartA.py
@@ -6,22 +6,42 @@ def tokenize(TextFilePath) -> list:
     O(n) to read each char 
     O(n) to change each char to lowercase
 
+    Reads in a text file and returns a list of the tokens in that file.
+    A token is a sequence of alphanumeric characters, independent of capitalization.
+
+    :param text_file_path: path to the text file to be read
+    :return: list of the tokens in the file
     '''
+    # Create empty list 'tokens' to store all tokens in text file.
     tokens = list()
     try:
-        with open(TextFilePath, 'r') as f:
-            text = f.read()
+        # Open the text file via the provided path and read from it byte-by-byte. ('rb')
+        with open(TextFilePath, 'rb') as f:
+            # Read 1 byte at a time
+            text = f.read(1) 
+            # Initialize the current token as an empty string.
             current = ''
-            for c in text: 
-                if (33 <= ord(c) <= 126):
-                    current += c.lower()
-                else:
-                    if current:
-                        tokens.append(current)
-                        current = ''
+            # While there are still valid bytes being read, complete the loop.
+            while text:
+                # Check each character in the byte
+                for c in text: 
+                    # Check ASCII code of the byte. In between 47 and 57 is a number, in between 65 and 122 is a character. 39 is apostrophe
+                    if (47 <= c <= 57) or (65 <= c <= 122) or (c == 39):
+                        # If valid ASCII character, append it to current token and convert to lowercase to avoid case-sensitivity.
+                        current += chr(c).lower()
+                    else:
+                        # Otherwise, the program has encountered a non-ASCII character. Stop parsing and add the current token as-is to the
+                        # token list.
+                        if current:
+                            tokens.append(str(current))
+                            # Reset token to empty string.
+                            current = ''
+                # Read the next byte.
+                text = f.read(1)
             if current:
-                tokens.append(current)
+                tokens.append(str(current))
     except Exception as e:
+        # Error handling for opening file path.
         print(f"Error opening file from {TextFilePath}: {str(e)}")
     
     return tokens
@@ -29,13 +49,22 @@ def tokenize(TextFilePath) -> list:
 def computeWordFrequencies(tokenList) -> dict:
     '''
     Runtime: O(n), iterates over n tokens
+
+    Counts number of occurences of each otken in token list and stores in a dictionary.
+
+    :param tokens: list of tokens
+    :return: dict, mapping of each token to number of occurrences.
     '''
+    # Initialize empty dictionary to store word frequencies.
     freq = {}
 
+    # For each token in the provided list, update how many times it appears in the dictionary.
     for token in tokenList:
         if token in freq:
+            # Token exists
             freq[token] += 1
         else:
+            # Token does not exist yet, add to dict
             freq[token] = 1
     
     return freq
@@ -46,17 +75,30 @@ def printFrequencies(wordCount):
     Runtime: O(nlogn)
     O(n) to iterate through the tokens and print
     O(nlogn) to sort the dictionary
+
+    Prints out word frequency count, ordered by decreasing frequency.
+
+    :param frequencies: dict, mapping each token to number of occurrences
     '''
+    # Create sorted dictionary, sorted by decreasing order.
     sort_freq = sorted(wordCount.items(), key=lambda x: (-x[1], x[0]), reverse=False)
+    # For each token-frequency pair, print out in correct format
     for token, freq in sort_freq:
         print(f"{token} -> {freq}")
 
 if __name__ == "__main__":
-    file_path = sys.argv[1]
-    tkns = tokenize(file_path)
-    #print(tkns)
-    tkn_dict = computeWordFrequencies(tkns)
-    #print(tkn_dict)
-    printFrequencies(tkn_dict)
+    try:
+        if len(sys.argv) < 1:
+            raise FileNotFoundError("Not enough file names provided.")
+        if len(sys.argv) > 2:
+            raise FileNotFoundError("Too many file names provided.")
+        file_path = sys.argv[1]
+        tkns = tokenize(file_path)
+        #print(tkns)
+        tkn_dict = computeWordFrequencies(tkns)
+        #print(tkn_dict)
+        printFrequencies(tkn_dict)
+    except Exception as e:
+        print(f"Error: {e}")
 
         
--- PartB.py
+++ PartB.py
@@ -1,16 +1,32 @@
 from PartA import tokenize, computeWordFrequencies
 import sys
 
-def countCommonTokens(file1_path, file2_path):
+def countCommonTokens(file1_tokens, file2_tokens) -> int:
     '''
     Runtime: O(n)
-    '''
-    tkns1 = computeWordFrequencies(tokenize(file1_path))
-    tkns2 = computeWordFrequencies(tokenize(file2_path))
 
-    common_tkns = set(tkns1.keys()) & set(tkns2.keys())
-    print(common_tkns)
+    Takes two token lists and performs set operations to filter common tokens from each list.
+
+    :param file1_tokens: list of tokens from file 1
+    :param file2_tokens: list of tokens from file 2
+    :return: length of resulting set, representative of number of common tokens
+    '''
+    # Get set union of file 1 and file 2 tokens
+    common_tkns = set(file1_tokens) & set(file2_tokens)
+    print(f"Common tokens: {common_tkns}")
+
+    # Return length of set union, aka the total amount of common tokens
     return len(common_tkns)
 
 if __name__ == "__main__":
-    print(countCommonTokens(sys.argv[1], sys.argv[2]))
+    try:
+        file1 = sys.argv[1]
+        file2 = sys.argv[2]
+    
+        tokens1 = tokenize(file1)
+        tokens2 = tokenize(file2)
+        print(countCommonTokens(tokens1, tokens2))
+    except Exception as e:
+        print(f"{e}: One or more of the files was not provided.")
+
+    
